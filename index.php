17df2d114e6d692feba50fa72e555002032a9407
Karthi <karthi.muthu95@gmail.com>
2017-02-21 10:49:26 +0530

    v1

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
 <title>HowTo</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/png" href="img/f.png" />	
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  <style>
  .navbar-inverse
  {
-     margin-bottom: 0;
-      background-color: #009999;
+     margin-bottom: 20px;
+      background-color:#175a84;
       z-index: 9999;
       border: 0;
       font-size: 12px !important;
@@ -20,20 +20,25 @@
       letter-spacing: 4px;
       border-radius: 0;
       font-family: Montserrat, sans-serif;
+	  padding-bottom: 30px;
  }
+}
  </style>
  </head>
 <body>
 <nav class="navbar navbar-inverse">
 <div class="container-fluid">
 <div class="navbar-header">
-<a class="navbar-brand">HowTO</a>
+<a class="navbar-brand"><img src="img/logo.png" width="150px" height="50px"></a>
+
+ <button  class="btn btn-success btn-md navbar-btn navbar-left">Sign in/Sign up</button>
+ 
 </div>
 </div>
 </nav>
-<div class="container">
+<div class="container-fluid">
 
-<iframe width="420" height="350" src=""></iframe>
+
 
 </div>
 
